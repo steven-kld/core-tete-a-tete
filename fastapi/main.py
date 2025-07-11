31e373d75f22610d17f4173a72a3482508c97c77
steven-kollo <steven.kollo.workspace@gmail.com>
2025-07-11 22:31:00 +0400

    working v1.1 - general sense filter & custom prompting per chat

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
+from atoms import init_openai
 from models import MessageRequest
 from config import CHAT_BLACKLIST
 from services import process_message
 load_dotenv()
 
 app = FastAPI()
+openai_client = init_openai()
 
 @app.post("/api/msg")
 async def receive_msg(data: MessageRequest):
     if data.group_name not in CHAT_BLACKLIST:
-        process_message(data)
+        process_message(data, openai_client)
         
     return JSONResponse(content={"status": "success", "message": "Data received successfully"}, status_code=200)
